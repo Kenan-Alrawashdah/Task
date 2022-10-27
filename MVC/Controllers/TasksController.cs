fa7853ab8e92639c2c1e255b8527d198eabf1aad
kenan-rawashdah <kenan.rawashdah@gmail.com>
2022-10-27 18:41:42 +0300

    implemant modal add task and fix  data task table

--- MVC/Controllers/TasksController.cs
+++ MVC/Controllers/TasksController.cs
@@ -1,13 +1,64 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.DTOs;
+using Application.IService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using Persistence.Entities.Constant;
 
 namespace MVC.Controllers
 {
     public class TasksController : Controller
     {
+        private readonly IEmployeeService _employeeService;
+        private readonly ITaskEntityService _taskEntityService;
+        public TasksController(IEmployeeService employeeService, ITaskEntityService taskEntityService)
+        {
+            _employeeService = employeeService;
+            _taskEntityService = taskEntityService;
+        }
         public ActionResult Index()
         {
-            return View();
+            var employees = _employeeService.GetAll().Select(emp => new EmployeeModel { 
+                ID = emp.ID, FirstName = emp.FirstName, LastName = emp.LastName 
+            }).ToList();
+
+            var status = Enum.GetValues(typeof(StatusTask)).Cast<StatusTask>().Select(sta => sta.ToString()).ToList();
+
+            var task = _taskEntityService.GetAll().Select(task => new TaskModel
+            {   
+                ID = task.ID,
+                Title = task.Title,
+                Description = task.Description,
+                ActualCost = task.ActualCost,
+                TotalBudget = task.TotalBudget,
+                StartDate = task.StartDate,
+                EndDate = task.EndDate,
+                ParentId = task.ParentId,
+                Status = task.Status,
+                Employees = task.Employees.Select(emp => new EmployeeModel { ID = emp.ID, PhotoName = emp.ImageUrl }).ToList(),
+                Children = task.Children.Select(sub => new TaskModel
+                {
+                    Title = sub.Title,
+                    Description = sub.Description,
+                    ActualCost = sub.ActualCost,
+                    TotalBudget = sub.TotalBudget,
+                    StartDate = sub.StartDate,
+                    EndDate = sub.EndDate,
+                    ParentId = sub.ParentId,
+                    Status = sub.Status,
+                    Employees = sub.Employees.Select(emp => new EmployeeModel { ID = emp.ID, PhotoName = emp.ImageUrl }).ToList(),
+                }).ToList(),
+            }).ToList();
+             
+            IndexViewModel indexViewModel = new IndexViewModel
+            {
+                Employees = employees,
+                Status = status,
+                Task = new TaskModel(),
+                Tasks = task,
+            };
+
+            return View(indexViewModel);
         }
 
         
@@ -19,15 +70,39 @@ namespace MVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(IndexViewModel model)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    var employees = model.Task?.SelectedEmployees?.Select(id => new EmployeeDto { ID = id }).ToList();
+                    var task = new TaskDto
+                    {
+                        Title = model.Task.Title,
+                        Description = model.Task.Description,
+                        ActualCost = model.Task.ActualCost,
+                        TotalBudget = model.Task.TotalBudget,
+                        StartDate = model.Task.StartDate,
+                        EndDate = model.Task.EndDate,
+                        ParentId = 13,
+                        Status = model.Task.Status,
+                        Employees = employees,
+
+                    };
+
+                    _taskEntityService.Add(task);
+
+                    return Json(new { success = true, message = "Sucssecful", status = 200 });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "test error", status = 400 });
+                }
             }
             catch
             {
-                return View();
+                return  Json(new { success = false, message = "test error", status = 500 });
             }
         }
 
---
+++ MVC/wwwroot/js/task.js
@@ -0,0 +1,30 @@
+﻿
+var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
+})
+
+const languages = $('#languages').filterMultiSelect({
+    placeholderText: "",
+});
+
+var formData = null
+
+$('#form').on('submit', (event) => {
+    event.preventDefault();
+    formData = new FormData($('#form').get(0));
+    JSON.parse(languages.getSelectedOptionsAsJson()).language.forEach(e => {
+        formData.append('Task.SelectedEmployees[]', +e)
+    });
+
+    $.ajax({
+        url: $('#form').attr('action'),
+        data: formData,
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        success: (res) => {
+            if (res.success) {
+                myModal.toggle();
+            } 
+        }
+    })
+})
